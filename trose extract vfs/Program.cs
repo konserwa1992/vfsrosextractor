66f07dd13e49eee694a6ab7208ddb00d27e2f6bc
konserwa1992 <38973532+konserwa1992@users.noreply.github.com>
2023-07-08 23:14:09 +0200

    first commit

---
+++ trose extract vfs/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+
+
+using System.Text;
+using trose_extract_vfs;
+
+
+Console.WriteLine("Path to rose:");
+string rosePath = Console.ReadLine();
+
+StreamReader dataStructureStream = new StreamReader(rosePath+"\\data.idx");
+StreamReader dataStream = new StreamReader(rosePath+"\\rose.vfs");
+
+string outPutFolder = "out";
+
+if (Directory.Exists(outPutFolder) == false)
+    Directory.CreateDirectory(outPutFolder);
+else
+    Console.WriteLine("out exist");
+
+//34
+int moveOffset = 1;
+byte[] buffer = new byte[255];
+
+
+
+
+
+while (!dataStructureStream.EndOfStream)
+{
+    chunkInfo fileInfo = new chunkInfo();
+
+   if(moveOffset==1)
+   moveOffset= (int)dataStructureStream.BaseStream.Seek(34, SeekOrigin.Begin);
+    dataStructureStream.BaseStream.Read(buffer, 0, 2);
+    ushort filePathTextSize = BitConverter.ToUInt16(buffer,0);
+    fileInfo.fileNameLength = filePathTextSize;
+
+    dataStructureStream.BaseStream.Read(buffer, 0, filePathTextSize);
+    string path = Encoding.ASCII.GetString(buffer, 0, filePathTextSize);
+    fileInfo.filePath = path;
+
+    dataStructureStream.BaseStream.Read(buffer, 0, 8);
+    fileInfo.fileOffset = BitConverter.ToUInt32(buffer, 0);
+    fileInfo.fileSize = BitConverter.ToUInt32(buffer, 4);
+
+    Console.WriteLine($"Path: {fileInfo.filePath}\nFile offset:{fileInfo.fileOffset.ToString("X")}\nFile size:{fileInfo.fileSize}");
+    dataStructureStream.BaseStream.Read(buffer, 0, 12);
+
+        if(fileInfo.filePath.LastIndexOf("/")!=-1)
+        Directory.CreateDirectory(fileInfo.filePath.Substring(0, fileInfo.filePath.LastIndexOf("/")));
+        FileStream file = File.Create(fileInfo.filePath);
+
+        byte[] fileRaw = new byte[fileInfo.fileSize];
+        dataStream.BaseStream.Seek(fileInfo.fileOffset, SeekOrigin.Begin);
+        dataStream.BaseStream.Read(fileRaw, 0, fileRaw.Length);
+        file.Write(fileRaw, 0, fileRaw.Length);
+        file.Flush();
+        file.Close();
+    
+}
+
+
---
+++ trose extract vfs/chunkInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trose_extract_vfs
+{
+    struct chunkInfo
+    {
+        public ushort fileNameLength;
+        public string filePath;
+        public uint fileOffset;
+        public uint fileSize;
+    };
+}
